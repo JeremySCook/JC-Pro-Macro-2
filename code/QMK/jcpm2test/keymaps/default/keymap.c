42e5ead8855073e93d4d38651bb06107ee4741b2
Jeremy Cook <jscook55@gmail.com>
2024-12-28 09:36:12 -0500

    Add files via upload

--- code/QMK/jcpm2test/keymaps/default/keymap.c
+++ code/QMK/jcpm2test/keymaps/default/keymap.c
@@ -1,21 +1,24 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
+#ifdef OLED_ENABLE
+
+
+
+#endif
 
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
-     * │ n │ n │ 6 │ 7 │
+     * │ 0 │ n │ 6 │ 7 │
      * ├───┼───┼───┼───┤
-     * │ n │ n │ 5 │ 8 │
+     * │ 1 │ n │ 5 │ 8 │
      * ├───┼───┼───┼───┤
      * │ 2 │ 3 │ 4 │ 9 │
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT(
         KC_P0, KC_P0, KC_P6, KC_P7,
-        KC_P0, KC_P0, KC_P5, KC_P8,
+        KC_P1, KC_P0, KC_P5, KC_P8,
         KC_P2, KC_P3, KC_P4, KC_P9
     )
 };
