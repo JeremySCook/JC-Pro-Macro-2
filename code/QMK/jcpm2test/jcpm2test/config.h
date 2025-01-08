2ecc4b3711f14125c3842514501ce7ba333c277f
Jeremy Cook <jscook55@gmail.com>
2025-01-07 21:03:46 -0500

    Add files via upload

---
+++ code/QMK/jcpm2test/jcpm2test/config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// I2C Configuration
+#define I2C_DRIVER I2CD1
+#define I2C1_SCL_PIN D0
+#define I2C1_SDA_PIN D1
+
+// OLED Configuration
+#define OLED_DISPLAY_128X64
+#define OLED_DISPLAY_ADDRESS 0x3C //correct per Arduino code
+#define OLED_TIMEOUT 2000000
+
+// Encoder Configuration
+//#define NUM_ENCODERS 1
+//#define NUM_DIRECTIONS 2
+#define ENCODER_RESOLUTION 4
+#define ENCODER_A_PINS { D2 }
+#define ENCODER_B_PINS { D3 }
+
+// LIGHTING Configuration
+#define WS2812_DI_PIN C6
+#define RGB_MATRIX_LED_COUNT 12
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_LIMIT_VAL 10
